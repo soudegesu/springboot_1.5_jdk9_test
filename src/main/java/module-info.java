b156f6824ff7b5fa48fb652e869171a85f4bcaf5
soudegesu <soudegesu@hotmail.co.jp>
2017-12-29 18:25:24 +0900

    build with module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,14 @@
 module soudegesu.springboot.jdk9.main {
     exports soudegesu.springboot.jdk9.main.demo;
+
+    requires static lombok;
+    requires java.xml.ws;
+    requires java.xml.ws.annotation;
+    requires java.xml.bind;
+
+    requires spring.web;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
 }
