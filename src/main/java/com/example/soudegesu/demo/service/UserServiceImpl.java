5b281ff9c7d711b5849c3e6813491e5aeba4b397
soudegesu <soudegesu@hotmail.co.jp>
2017-12-29 17:52:41 +0900

    set sample data

--- src/main/java/com/example/soudegesu/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/soudegesu/demo/service/UserServiceImpl.java
@@ -13,7 +13,8 @@ public class UserServiceImpl implements UserService {
     public List<User> getUsers() {
         List<User> users = new ArrayList<>();
         User user = new User();
-
+        user.setId(1);
+        user.setName("hoge");
         users.add(user);
         return users;
     }
