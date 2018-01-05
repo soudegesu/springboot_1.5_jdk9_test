1f095483271a3b39ea07ef93929c27bcade1347f
soudegesu <soudegesu@hotmail.co.jp>
2018-01-05 10:11:52 +0900

    change to open module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module soudegesu.springboot.jdknine.main.demo {
+open module soudegesu.springboot.jdknine.main.demo {
 
 //    requires static lombok;
 //    requires java.xml.ws;
@@ -10,9 +10,9 @@ module soudegesu.springboot.jdknine.main.demo {
     requires spring.boot.autoconfigure;
     requires spring.boot;
 
-    opens soudegesu.springboot.jdknine.main.demo;
-    opens soudegesu.springboot.jdknine.main.demo.controller;
-    opens soudegesu.springboot.jdknine.main.demo.model;
-    opens soudegesu.springboot.jdknine.main.demo.service;
-    exports soudegesu.springboot.jdknine.main.demo;
+//    opens soudegesu.springboot.jdknine.main.demo;
+//    opens soudegesu.springboot.jdknine.main.demo.controller;
+//    opens soudegesu.springboot.jdknine.main.demo.model;
+//    opens soudegesu.springboot.jdknine.main.demo.service;
+//    exports soudegesu.springboot.jdknine.main.demo;
 }
